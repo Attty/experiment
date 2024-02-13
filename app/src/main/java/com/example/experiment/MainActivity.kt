a460256ae1b16f616ee5def05aabafdc97b582a2
vlad <10327965@stud.op.edu.ua>
2024-02-14 00:31:11 +0200

    aboba

--- app/src/main/java/com/example/experiment/MainActivity.kt
+++ app/src/main/java/com/example/experiment/MainActivity.kt
@@ -3,15 +3,13 @@ package com.example.experiment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Aboba", modifier = Modifier.fillMaxSize())
+
         }
     }
 }
