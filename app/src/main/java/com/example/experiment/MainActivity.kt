873f0726701901955c92cb41e767484764aa5ad6
vlad <10327965@stud.op.edu.ua>
2024-02-14 01:30:37 +0200

    aboba

--- app/src/main/java/com/example/experiment/MainActivity.kt
+++ app/src/main/java/com/example/experiment/MainActivity.kt
@@ -1,15 +1,181 @@
 package com.example.experiment
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            App()
         }
     }
 }
+
+@Composable
+fun App() {
+    var selectedImageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    var nickname by remember {
+        mutableStateOf("nickname")
+    }
+    val navController = rememberNavController()
+
+    val photoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri ->
+            selectedImageUri = uri
+        }
+    )
+    NavHost(
+        navController = navController,
+        startDestination = "main_screen"
+    ) {
+        composable("main_screen") {
+            MainScreen(image = selectedImageUri, nickname = nickname, onEditButtonClick = {
+                navController.navigate("edit_screen")
+            })
+        }
+        composable("edit_screen") {
+            EditScreen(image = selectedImageUri, nickname = nickname,
+                onBackButtonClick = {
+                    navController.popBackStack()
+                },
+                onImageClick = {
+                    photoPickerLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                },
+                onTextChange = {
+                    nickname = it
+                },
+                onSaveButtonClick = {
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditScreen(
+    image: Uri?,
+    nickname: String,
+    onBackButtonClick: () -> Unit,
+    onImageClick: () -> Unit,
+    onTextChange: (String) -> Unit,
+    onSaveButtonClick: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Edit Screen") },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        onBackButtonClick.invoke()
+                    }) {
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "")
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Column {
+                    AsyncImage(
+                        model = image,
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(128.dp)
+                            .clip(CircleShape)
+                            .clickable {
+                                onImageClick.invoke()
+                            })
+                Row(horizontalArrangement = Arrangement.Center){
+                    BasicTextField(value = nickname, onValueChange = {
+                        onTextChange(it)
+                    })
+                }
+
+            }
+            Button(onClick = { onSaveButtonClick.invoke() }) {
+                Text("Save")
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(
+    image: Uri?,
+    nickname: String,
+    onEditButtonClick: () -> Unit
+) {
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+            Column {
+                    AsyncImage(
+                        model = image,
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(128.dp)
+                            .clip(CircleShape)
+                    )
+                Row(horizontalArrangement = Arrangement.Center) {
+                    Text(nickname)
+                }
+            }
+            Button(onClick = { onEditButtonClick.invoke() }) {
+                Text("Edit")
+            }
+        }
+    }
